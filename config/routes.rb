ddfb92bce7632c3921b649eb73f63154e7d72b09
okasha <abdallahokasha>
2019-10-26 07:03:48 +0200

    Added nested routes.
    - application chats.
    - chat_messages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :messages
-  resources :chats
-  resources :applications
+  resources :chats do
+    resources :messages
+  end  
+  resources :applications do 
+    resources :chats
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
